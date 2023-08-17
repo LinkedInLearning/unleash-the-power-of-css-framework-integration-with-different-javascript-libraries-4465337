8d8554a5e56a163729c6ee520f14876225146d65
Ayodele Aransiola <36636824+CodeLeom@users.noreply.github.com>
2023-08-17 03:11:45 +0100

    style: template vue with vite and install bulma into it

---
+++ 03_06/my-vue-app/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="notification is-danger">
+    <button class="delete"></button>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit lorem ipsum dolor sit amet, consectetur adipiscing elit
+  </div>
+</template>
+
+<style scoped>
+
+</style>
