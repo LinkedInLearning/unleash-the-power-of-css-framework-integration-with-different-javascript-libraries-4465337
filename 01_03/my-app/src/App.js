a3ac9032bf07a91491c91f2f456b676353004263
Ayodele Aransiola <36636824+CodeLeom@users.noreply.github.com>
2023-08-21 04:26:09 +0100

    style: updated components

--- 01_03/my-app/src/App.js
+++ 01_03/my-app/src/App.js
@@ -1,8 +1,16 @@
+import Blog from './layout/Blog';
+import Footer from './layout/Footer';
+import Hero from './layout/Hero'
+import Stats from './layout/Stats';
 
 function App() {
   return (
-     <>
-     </>
+    <>
+      <Hero />
+      <Stats />
+      <Blog />
+      <Footer />
+    </>
   );
 }
 
