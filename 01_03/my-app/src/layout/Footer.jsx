5f5ec7080a21dd3c4d544364f0a775d7dffb7a44
Ayodele Aransiola <36636824+CodeLeom@users.noreply.github.com>
2023-08-23 05:01:04 +0100

    style: updated components

--- 01_03/my-app/src/layout/Footer.jsx
+++ 01_03/my-app/src/layout/Footer.jsx
@@ -6,7 +6,7 @@ export default function Footer() {
     <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
         <div className="md:flex md:justify-between">
           <div className="mb-6 md:mb-0">
-              <a href="https://flowbite.com/" className="flex items-center">
+              <a href="!#" className="flex items-center">
                   <img src="logo192.png" className="h-8 mr-3" alt="site Logo" />
                   <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Site-Name</span>
               </a>
